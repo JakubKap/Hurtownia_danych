baf0166f78f0204d460e6104921aadaef368ffdb
Jakub Kapusta <jakubkapusta39@gmail.com>
2019-05-11 20:35:48 +0200

    Dodanie pakietu, przypisującego w tabeli faktów opowiednie Id linii lotniczej

---
+++ ssis_hd/sql/setting atributes_of_fact_table_to_nullable.sql
@@ -0,0 +1,27 @@
+ALTER TABLE FactFlightActivity
+ALTER COLUMN LocalScheduledDepartureTime int NULL;
+ALTER TABLE FactFlightActivity
+ALTER COLUMN UniversalScheduledArrivalTime int NULL;
+ALTER TABLE FactFlightActivity
+ALTER COLUMN UniversalScheduledDepartureTime int NULL;
+ALTER TABLE FactFlightActivity
+ALTER COLUMN OperatingAirline int NULL;
+ALTER TABLE FactFlightActivity
+ALTER COLUMN UniversalActualArrivalTime int NULL;
+ALTER TABLE FactFlightActivity
+ALTER COLUMN LocalActualDepartureTime int NULL;
+ALTER TABLE FactFlightActivity
+ALTER COLUMN LocalScheduledArrivalTime int NULL;
+ALTER TABLE FactFlightActivity
+ALTER COLUMN ArrAirport int NULL;
+ALTER TABLE FactFlightActivity
+ALTER COLUMN FlightDate int NULL;
+ALTER TABLE FactFlightActivity
+ALTER COLUMN UniversalActualDepartureTime int NULL;
+ALTER TABLE FactFlightActivity
+ALTER COLUMN DepAirport int NULL;
+ALTER TABLE FactFlightActivity
+ALTER COLUMN LocalActualArrivalTime int NULL;
+
+
+
