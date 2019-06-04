6698856c67c3c11bfa2032f47245bda38a7a5afc
Jakub Kapusta <jakubkapusta39@gmail.com>
2019-06-04 13:21:22 +0200

    Dodanie czyszczenia tabeli faktów ze Stage'a

--- ssis_hd/sql/Adam Kochański/getCancellationId_func_dwh.sql
+++ ssis_hd/sql/Adam Kochański/getCancellationId_func_dwh.sql
@@ -1,14 +1,23 @@
-CREATE FUNCTION dbo.getCancellationId(
+USE [AirStatsDWH]
+GO
+/****** Object:  UserDefinedFunction [dbo].[getCancellationId]    Script Date: 04.06.2019 13:15:55 ******/
+SET ANSI_NULLS ON
+GO
+SET QUOTED_IDENTIFIER ON
+GO
+ALTER FUNCTION [dbo].[getCancellationId](
 @CancellationKey varchar(5))
 RETURNS int
 AS
 BEGIN
 
+
 DECLARE @maxKey int= (SELECT MAX(CancellationID) FROM DimCancellationReason)
 
+IF @CancellationKey is NULL RETURN @maxKey
 IF (SELECT CASE WHEN COUNT(1)!=1 THEN 0 ELSE 1 END 
 FROM DimCancellationReason 
-WHERE CancellationReasonKey=@CancellationKey)=1 RETURN @maxKey
+WHERE CancellationReasonKey=@CancellationKey)=1  RETURN @maxKey
 
 return (SELECT CancellationID FROM DimCancellationReason WHERE CancellationReasonKey=@CancellationKey)
 
