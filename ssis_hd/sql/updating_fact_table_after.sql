9413aced994e79096fdfd4427fb2e3084be9193c
Jakub Kapusta <jakubkapusta39@gmail.com>
2019-06-04 17:19:41 +0200

    Zasilenie pierwszych 1000 faktów

---
+++ ssis_hd/sql/updating_fact_table_after.sql
@@ -0,0 +1,92 @@
+UPDATE dbo.TempFactFlightActivity SET LocalScheduledDepartureTime = NULL
+   WHERE LocalScheduledDepartureTime=-9999;
+
+   
+      UPDATE dbo.TempFactFlightActivity SET DepDelayGroup = NULL WHERE DepDelayGroup=-9999;
+
+	  
+      UPDATE dbo.TempFactFlightActivity SET DifficultiesKey = NULL WHERE DifficultiesKey=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET UniversalScheduledArrivalTime = NULL WHERE UniversalScheduledArrivalTime=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET OperatingAirline = NULL WHERE OperatingAirline=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET UniversalActualArrivalTime = NULL WHERE UniversalActualArrivalTime=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET UniversalActualArrivalDate = NULL WHERE UniversalActualArrivalDate=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET LocalActualDepartureTime = NULL WHERE LocalActualDepartureTime=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET LocalScheduledDepartureDate = NULL WHERE LocalScheduledDepartureDate=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET UniversalScheduledDepartureTime = NULL WHERE UniversalScheduledDepartureTime=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET LocalScheduledArrivalTime = NULL WHERE LocalScheduledArrivalTime=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET CancellationCode = NULL WHERE CancellationCode=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET UniversalScheduledArrivalDate = NULL WHERE UniversalScheduledArrivalDate=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET ArrAirport = NULL WHERE ArrAirport=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET UniversalActualDepartureDate = NULL WHERE UniversalActualDepartureDate=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET FlightDate = NULL WHERE FlightDate=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET UniversalActualDepartureTime = NULL WHERE UniversalActualDepartureTime=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET DepAirport = NULL WHERE DepAirport=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET LocalActualDepartureDate = NULL WHERE LocalActualDepartureDate=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET LocalScheduledArrivalDate = NULL WHERE LocalScheduledArrivalDate=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET UniversalScheduledDepartureDate = NULL WHERE UniversalScheduledDepartureDate=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET ArrTimeBLK = NULL WHERE ArrTimeBLK=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET DepTimeBLK = NULL WHERE DepTimeBLK=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET LocalActualArrivalTime = NULL WHERE LocalActualArrivalTime=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET LocalActualArrivalDate = NULL WHERE LocalActualArrivalDate=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET TailNumber = NULL WHERE TailNumber='-9999';
+
+      UPDATE dbo.TempFactFlightActivity SET FlightNumber = NULL WHERE FlightNumber='-9999';
+
+      UPDATE dbo.TempFactFlightActivity SET DepDelay = NULL WHERE DepDelay=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET DepDelayMinutes = NULL WHERE DepDelayMinutes=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET TaxiOut = NULL WHERE TaxiOut=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET TaxiIn = NULL WHERE TaxiIn=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET ArrDelay = NULL WHERE ArrDelay=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET ArrDelayMinutes = NULL WHERE ArrDelayMinutes=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET ArrDelayGroup = NULL WHERE ArrDelayGroup=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET SchedElapsedTime = NULL WHERE SchedElapsedTime=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET ActualElapsedTime = NULL WHERE ActualElapsedTime=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET AirTime = NULL WHERE AirTime=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET Flights = NULL WHERE Flights=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET Distance = NULL WHERE Distance=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET CarrierDelay = NULL WHERE CarrierDelay=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET WeatherDelay = NULL WHERE WeatherDelay=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET NASDelay = NULL WHERE NASDelay=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET SecurityDelay = NULL WHERE SecurityDelay=-9999;
+
+      UPDATE dbo.TempFactFlightActivity SET LateAircraftDelay = NULL WHERE LateAircraftDelay=-9999;
+
+ 
